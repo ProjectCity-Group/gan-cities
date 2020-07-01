396a383cb57f280d91c105d15264143e4107a810
Thomas Allen <thomallen96@gmail.com>
2020-07-01 22:59:35 +0000

    Add setup files.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'citygan',
+  packages = ['citygan'],
+  version = '0.1',
+  license='MIT',
+  description = 'Procedurally generated cities using generative adversarial networks',
+  author = 'Thomas Allen, Eric Lehmann, Jacob Roberge, Christopher Ward, Adrian Wright',
+  url = 'https://github.com/ProjectCity-Group/gan-cities',
+  download_url = 'https://github.com/ProjectCity-Group/gan-cities/archive/v0.1.tar.gz',
+  keywords = ['gan', 'city', 'procedural', 'generation'],
+  install_requires=[
+        'wheel',
+        'imageio',
+        'keras',
+        'matplotlib'
+        'numpy'
+        'tensorflow'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
+
